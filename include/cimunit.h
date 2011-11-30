6ac8ce459e011e596d6a27b799e5b3ae8cd6642e
Dale <github@gleannfionn.com>
2011-11-29 21:51:58 -0600

    ADD: Main page to Doxygen documentation

--- include/cimunit.h
+++ include/cimunit.h
@@ -23,18 +23,18 @@
 
 #include "cimunit_schedule.h"
 
-/// \addtogroup cimunit_user CIMUnit Test Methods 
 
 /// Global CIMUnit variable used by the CIMUnit test macros.
 extern struct cimunit_schedule *g_cimunit_default_schedule;
 
+/// \addtogroup cimunit CIMUnit Module
+/// @{
 
 /// Create a CIMUnit schedule
 ///
 /// This differs from cimunit_schedule_parse by using a global variable that
 /// works with the other CIMUnit macros to simplify test development in an
 /// environment where only one test will be run at a time.
-/// \ingroup cimunit_user
 #define CIMUNIT_SCHEDULE(schedule) \
     if (g_cimunit_default_schedule) { \
         cimunit_schedule_destroy(g_cimunit_default_schedule); \
@@ -46,7 +46,6 @@ extern struct cimunit_schedule *g_cimunit_default_schedule;
 ///
 /// This differs from cimunit_event_fire as it uses a global variable to
 /// simplify test development.
-/// \ingroup cimunit_user
 #define CIMUNIT_FIRE(eventName) \
     cimunit_schedule_fire(g_cimunit_default_schedule, eventName)
 
@@ -54,10 +53,11 @@ extern struct cimunit_schedule *g_cimunit_default_schedule;
 /// Name thread in the schedule
 ///
 /// This macro gives a name to the currently executing thread
-/// \ingroup cimunit_user
 #define CIMUNIT_THREAD_NAME(threadName) \
     cimunit_schedule_set_thread_name(g_cimunit_default_schedule, \
                                      cimunit_thread_self(), \
                                      threadName)
+
+/// @}
 
 #endif //CIMUNIT_H
